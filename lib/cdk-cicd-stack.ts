79f36d13992d15b2102312982105834164ec163a
sycomorely <1356744946@qq.com>
2025-01-22 14:30:42 +1000

    add test stage

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, ShellStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'ChuckPipeline', {
+    const pipeline = new CodePipeline(this, 'ChuckPipeline', {
       pipelineName: 'ChuckPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('sycomorely/cdk-practice', 'main'),
@@ -17,5 +18,9 @@ export class CdkCicdStack extends cdk.Stack {
         ],
       }),
     });
+
+    const teststage = pipeline.addStage(new PipelineStage(this,'Teststage',{
+      stageName:'test'
+    }))
   }
 }
